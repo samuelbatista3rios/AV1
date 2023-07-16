dd7ec6d85e90c3f65ca0cb28d0998af7e8baf045
samuelbatista3rios <samuelbatista3rios@gmail.com>
2023-07-15 22:26:54 -0300

    commit final

---
+++ backend/models.py
@@ -0,0 +1,56 @@
+from exts import db 
+
+
+"""
+class Recipe:
+   id:int primary key
+   title:str
+   description:str (text)
+    
+"""    
+
+class Recipe(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    title=db.Column(db.String(),nullable=False)
+    description=db.Column(db.Text(),nullable=False)
+    
+    def __repr__(self):
+        return f"<Recipe {self.title}>"
+    
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def delete(self):
+        db.session.delete(self)    
+        db.session.commit()
+        
+    def update(self,title,description):
+        self.title = title
+        self.description = description                                                                                                                                                                                                                                                                                  
+        db.session.commit()
+        
+    def query_all(self):
+         recipes = db.session.query(Recipe).all()
+         return recipes
+ 
+#user model
+"""
+class User:
+    id:interger
+    username:string
+    email:string  
+    password:string
+"""
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(255),nullable=False)
+    email=db.Column(db.String(80),nullable=False)   
+    passwords=db.Column(db.Text(),nullable=False)
+    
+    def __repr__(self):
+        return f"<User {self.username}>"
+    
+                        
